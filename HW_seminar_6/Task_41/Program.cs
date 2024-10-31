38229ffe49ee413c0e6e4e590906954eaa3d4584
EGOR_QQ <oopyatkinoo@mail.ru>
2024-10-31 21:19:58 +0300

    Решил домашку к 6 семинару

---
+++ HW_seminar_6/Task_41/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+string num1 = "";
+int count = 0;
+Console.WriteLine($"Вводите числа, когда закончите, введите 'stop':");
+for (int i = 0; i < 999; i++)
+{
+    num1 = Console.ReadLine();
+    if (num1 != "stop")
+    {
+        Convert.ToInt32(num1);
+        count++;
+    }
+    else
+    {
+        break;
+    }
+}
+Console.Write($"Из введенных чиcел - {count} положительных!");
---
+++ HW_seminar_6/Task_43/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+
+Console.WriteLine($"Вводите значения k1, b1: ");
+float k1 = ReadNum();
+float b1 = ReadNum();
+Console.WriteLine($"Вводите значения k2, b2: ");
+float k2 = ReadNum();
+float b2 = ReadNum();
+
+Console.Write($"Для того, чтобы найти точку пересечения прямых y = {k1}*x + {b1} и y = {k2}*x + {b2} введите 'YES': ");
+string result = Console.ReadLine();
+float x = 0;
+float y = 0;
+if (result == "YES")
+{
+    x = ((b2 - b1) / (k1 - k2));
+    y = (x*k1 + b1);
+    Console.WriteLine($"Точка пересечения прямых y = {k1}*x + {b1} и y = {k2}*x + {b2} - ({x}:{y}):");
+}
+
+
+//ввод чисел int
+float ReadNum()
+{
+    float i = Convert.ToInt32(Console.ReadLine());
+    return i;
+}
